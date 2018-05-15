b9c9edd5e1e94f52347e70d8d2940f339803a91e
naebliss <naebliss666@hotmail.com>
2018-05-15 09:21:53 +0200

    initial commit

---
+++ IdentityMvcWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(IdentityMvcWeb.Startup))]
+namespace IdentityMvcWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
