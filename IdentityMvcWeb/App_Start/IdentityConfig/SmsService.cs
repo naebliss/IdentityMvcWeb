d76d7a02b30373f26429f05f89f531cc21c9aa5c
naebliss <naebliss666@hotmail.com>
2018-05-16 09:23:19 +0200

    add email service for two-factor sms authentication

--- IdentityMvcWeb/App_Start/IdentityConfig/SmsService.cs
+++ IdentityMvcWeb/App_Start/IdentityConfig/SmsService.cs
@@ -1,5 +1,10 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Configuration;
+using System.Diagnostics;
+using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
 
 namespace IdentityMvcWeb.IdentityConfig
 {
@@ -7,7 +12,24 @@ namespace IdentityMvcWeb.IdentityConfig
     {
         public Task SendAsync(IdentityMessage message)
         {
-            // Plug in your SMS service here to send a text message.
+            string twilioSid = ConfigurationManager.AppSettings["TwilioSid"];
+            string twilioApiKey = ConfigurationManager.AppSettings["TwilioApiKey"];
+            string twilioFromNr = ConfigurationManager.AppSettings["TwilioFromNr"];
+
+            TwilioClient.Init(twilioSid, twilioApiKey);
+
+            var fromNr = new Twilio.Types.PhoneNumber(twilioFromNr);
+            var toNr = new Twilio.Types.PhoneNumber(message.Destination);
+            var sms = MessageResource.Create(
+                body: message.Body,
+                from: fromNr,
+                to: toNr,
+                pathAccountSid: twilioSid
+            );
+
+            Trace.TraceInformation(sms.Status.ToString());
+
+            // Twilio doesn't currently have an async API, so we return success.
             return Task.FromResult(0);
         }
     }
